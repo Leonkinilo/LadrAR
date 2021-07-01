4982d7c0c577757ac8b5d4bbec4477d7c0cb6e21
Leonkinilo <leonkinilo@gmail.com>
2021-07-01 19:53:52 -0300

    adelantando a tu vieja

--- Funciones/perros.funciones.js
+++ Funciones/perros.funciones.js
@@ -3,6 +3,9 @@ const perros = [];
 perros.Crear = async (req, res) => {
 res.send("juan")
 }
+perros.Metodoget = async (req, res) => {
+res.json({nombre: 'juan', apellido: 'juancito'})
+}
 
 
 module.exports = perros;
--- routes/perros.rutas.js
+++ routes/perros.rutas.js
@@ -4,5 +4,6 @@ const rutas = express.Router();
 const perros = require ('../Funciones/perros.funciones');
 
 rutas.post('/', perros.Crear);
+rutas.get('/', perros.Metodoget);
 
 module.exports = rutas;
