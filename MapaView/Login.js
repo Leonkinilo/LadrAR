2df1a8e6a3c129e2eb1f12824663c44581b85d0a
Brassica-oleracea-var-italica <83074563+Brassica-oleracea-var-italica@users.noreply.github.com>
2021-08-13 10:38:15 -0300

    Add files via upload

---
+++ MapaView/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+
+export default () => (
+  <View>
+    <Text></Text>
+    <Button title="" onPress={() => 5}/>
+  </View>
+)
