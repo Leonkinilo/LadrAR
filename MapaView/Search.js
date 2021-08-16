0b9281b883bd851541303e2acca4d4c68193468f
Brassica-oleracea-var-italica <83074563+Brassica-oleracea-var-italica@users.noreply.github.com>
2021-08-16 18:14:02 -0300

    Update Search.js

--- MapaView/Search.js
+++ MapaView/Search.js
@@ -7,23 +7,56 @@ import {
   Alert,
   Image,
   TouchableOpacity,
-  Dimensions,
   TextInput,
 } from 'react-native';
 
 export default ({ history }) => (
   <View style={styles.container}>
-    <Text>Has tus busqueda aqui</Text>
-    <TouchableOpacity onPress={() => history.push('/Login')}>
-      <Text>Change page</Text>
-    </TouchableOpacity>
+    <View style={styles.blancocontainer}>
+      <View style={styles.blanco}>
+        <TextInput style={styles.tama}/>
+      </View>
+
+      <View style={styles.blanco}>
+        <TextInput style={styles.tama}/>
+      </View>
+
+      <View style={styles.blanco}>
+        <TextInput style={styles.tama}/>
+      </View>
+
+      <View style={styles.blanco}>
+        <TextInput style={styles.tama}/>
+      </View>
+
+      <View style={styles.blanco}>
+        <TextInput style={styles.tama}/>
+      </View>
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor:"#999",
+  container: {
+    backgroundColor: '#fff',
     padding: 2,
-  }
-
+    flexDirection: 'colum',
+  },
+  blancocontainer: {
+    position: 'relative',
+    top: -10,
+  },
+  blanco: {
+    width: '100%',
+    height: 110,
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  tama:{
+    borderWidth: 2,
+    borderRadius: 0,
+    borderColor: '#CCC',
+    height:'100%',
+    width: '99%',
+  },
 });
