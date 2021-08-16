6343b057123b83577594f7418c8338f2d8ce6400
Brassica-oleracea-var-italica <83074563+Brassica-oleracea-var-italica@users.noreply.github.com>
2021-08-16 19:52:38 -0300

    Update Search.js

--- MapaView/Search.js
+++ MapaView/Search.js
@@ -14,24 +14,41 @@ export default ({ history }) => (
   <View style={styles.container}>
     <View style={styles.blancocontainer}>
       <View style={styles.blanco}>
-        <TextInput style={styles.tama}/>
+        <TextInput style={styles.tama} />
       </View>
 
       <View style={styles.blanco}>
-        <TextInput style={styles.tama}/>
+        <TextInput style={styles.tama} />
       </View>
 
       <View style={styles.blanco}>
-        <TextInput style={styles.tama}/>
+        <TextInput style={styles.tama} />
       </View>
 
       <View style={styles.blanco}>
-        <TextInput style={styles.tama}/>
+        <TextInput style={styles.tama} />
       </View>
 
       <View style={styles.blanco}>
-        <TextInput style={styles.tama}/>
+        <TextInput style={styles.tama} />
       </View>
+    </View>
+    <View style={styles.nav}>
+      <TouchableOpacity
+        onPress={() => history.push('/Search')}
+        style={styles.navItem}>
+        <Text style={styles.butsize}>Search</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => history.push('/Home')}
+        style={styles.navItem}>
+        <Text style={styles.butsize}>Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => history.push('/Sigup')}
+        style={styles.navItem}>
+        <Text style={styles.butsize}>Sigup</Text>
+      </TouchableOpacity>
     </View>
   </View>
 );
@@ -39,7 +56,7 @@ export default ({ history }) => (
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
-    padding: 2,
+    padding: 0,
     flexDirection: 'colum',
   },
   blancocontainer: {
@@ -47,16 +64,35 @@ const styles = StyleSheet.create({
     top: -10,
   },
   blanco: {
+    padding: 2,
     width: '100%',
     height: 110,
     justifyContent: 'center',
     textAlign: 'center',
   },
-  tama:{
+  tama: {
     borderWidth: 2,
     borderRadius: 0,
     borderColor: '#CCC',
-    height:'100%',
+    height: '100%',
     width: '99%',
+  },
+
+  nav: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    backgroundColor: '#FC4C00',
+    position: 'relative',
+    top: 18,
+    height: 45,
+    width:'100%',
+    alignItems: 'center',
+  },
+  navItem: {
+    alignItems: 'center',
+    //flex:1,
+  },
+  butsize: {
+    fontSize: 20,
   },
 });
