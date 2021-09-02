3964a8b9b06f8ac5c032a98f54e63e781acd5b37
juliestrugo <julietaestrugo@gmail.com>
2021-09-02 10:02:29 -0300

    prueba

--- MapaView/Conexion/getMarkerCoordenadas.js
+++ MapaView/Conexion/getMarkerCoordenadas.js
@@ -11,7 +11,7 @@ import {
   Dimensions,
   FlatList,
 } from 'react-native';
-import React, {Component} from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import MapView from 'react-native-maps';
 import { Marker, Polyline, Polygon, } from 'react-native-maps';
@@ -22,63 +22,64 @@ componentWillonMount
 componentWillUpdate
 */
 
-    // post
-    // get
-    // put
-    // delete
+// post
+// get
+// put
+// delete
 
 export default class Putcoordenadas extends Component {
-    state = {
-      nombres: [],
-      noombre: "",
-      longitud: null,
-      latitud: null,
-      descripcion: "",
+  state = {
+    nombres: [],
+    noombre: "",
+    longitud: null,
+    latitud: null,
+    descripcion: "",
 
-    }
+  }
 
-    }
 
-    async componentDidMount () {
-      conts datos = await axios.get("https://ladr-ar.herokuapp.com/home")
-      this.setState({nombres: datos.data}) //ver que es data u otro da
-      console.log(this.state.nombres)
 
-    }
+  async componentDidMount() {
+    const datos = await axios.get("https://ladr-ar.herokuapp.com/home")
+    this.setState({ nombres: datos.data }) //ver que es data u otro da
+    console.log(this.state.nombres)
 
-    render() {
-      return(
-        <View style={styles.container}>
-          <View style={styles.mapcontainer}>
-            <MapView
-              style={styles.map}
-              region={{
-                latitude: -34.54977,
-                longitude: -58.453956,
-                latitudeDelta: 0.015,
-                longitudeDelta: 0.0121,
-              }}>
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.mapcontainer}>
+          <MapView
+            style={styles.map}
+            region={{
+              latitude: -34.54977,
+              longitude: -58.453956,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}>
 
             <View>
               {
                 this.state.nombres.map(nombres => (
                   <Marker
-                    coordinate=({
+                    coordinate={{
                       latitude: parseFloat(nombres.latitud),
                       longitude: parseFloat(nombres.longitud),
-                    })
+                    }}
                     title={nombres.nombre}
                     description={nombres.descripcion}
                   />
-              }
+                ))}
+
             </View>
 
-            </MapView>
-          </View>
-          
+          </MapView>
         </View>
-      )
-    }
+
+      </View>
+    )
+  }
 
 
 }
@@ -87,4 +88,5 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     padding: 0,
-  },
+  }
+})
