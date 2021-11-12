a7180ad86bcc9dbda148d7478975d924d5d50c9b
juliestrugo <julietaestrugo@gmail.com>
2021-11-12 10:15:30 -0300

    get funcionando

--- front-end/src/componentes/home.js
+++ front-end/src/componentes/home.js
@@ -1,37 +1,39 @@
-import React, { Component, useState } from "react";
-import { Card,Button } from "react-bootstrap";
+import React, { Component, useEffect, useState } from "react";
+import { Card, Button } from "react-bootstrap";
 import axios from "axios";
 // const API = axios.create({
 //   baseURL: `${"http://ladr-ar.herokuapp.com/home"}`
 // });
 
-async function getdatos(){
-  const response = await axios.get("http://ladr-ar.herokuapp.com/home", {
-  });
-  return response;
-}
-const data = getdatos();
-
-export default function home() {
-
-//  try {
-//    getdatos();
-//  } catch (error) {
-//    console.log(error)
-//  }
 
 
-
+export default function Home() {
+  const [data, setData] = useState([])
+  async function getdatos() {
+    const response = await axios.get("https://ladr-ar.herokuapp.com/home");
+    setData(response.data)
+    console.log(response.data);
+  }
+  useEffect(() => {
+    getdatos();
+  }, [])
   return (
 
     <div>
-      <Button onClick={()=>
-      console.log(getdatos({data}))
-      }></Button>
-     
-      <h1>tu re vieja</h1>
+      {data.length !== 0 && data.map((perrito, i) => (
+        <div>
+          <p>Perrito {i} </p>
+          <p>Nombre: {perrito.nombre}</p>
+          <p>Tipo: {perrito.tipo}</p>
+          <p>Descripcion: {perrito.descripcion}</p>
+          <p>Latitud: {perrito.latitud}</p>
+          <p>Longitud: {perrito.longitud}</p>
+          <p>Creador: {perrito.creador}</p>
+          <p>Contacto: {perrito.contacto}</p>
+          <br/>
+          <br/>
+        </div>
+      ))}
     </div>
   )
 }
-
-
