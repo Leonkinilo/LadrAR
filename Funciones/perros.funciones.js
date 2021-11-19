45a9577abc4d32dd4cfcad6782e2cdfcf2732dc4
juliestrugo <julietaestrugo@gmail.com>
2021-11-18 23:11:30 -0300

    aaaa

--- Funciones/perros.funciones.js
+++ Funciones/perros.funciones.js
@@ -24,7 +24,7 @@ perros.BuscarTodas = async (req, res) => {
     
 }
 perros.Buscar = async (req, res) => {
-    const ubicacion = await Models.find({"id": req.params.id});
+    const ubicacion = await Models.findById(req.params.id);
     res.json(ubicacion);
 }
 
